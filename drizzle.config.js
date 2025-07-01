c46d5529b5992c52a012520c5b348fc6722a1dad
SoniaSharma15 <148994808+SoniaSharma15@users.noreply.github.com>
2025-07-01 17:21:23 +0530

    update database

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default{
     schema:"./utils/Schema.js",
     dialect:'postgresql',
     dbCredentials:{
-     url:"postgresql://neondb_owner:npg_aB2gWusLQ9bO@ep-patient-bar-a1yvftf7.ap-southeast-1.aws.neon.tech/Ai%20mockInterview?sslmode=require&channel_binding=require"
+     url:"postgresql://neondb_owner:npg_IL5SiQfyqx3d@ep-morning-sky-a8bzivvo-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require"
     }
 }
