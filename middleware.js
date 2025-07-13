e621eb50fbe164456ce230137baccaf2c902abdc
SoniaSharma15 <148994808+SoniaSharma15@users.noreply.github.com>
2025-07-13 16:10:26 +0530

    bug fix

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)','/softSkills(.*)', '/resume(.*)','/forum(.*)'])
+  '/dashboard(.*)','/softSkills(.*)','/forum(.*)'])
 
 
 export default clerkMiddleware(async (auth, req) => {
